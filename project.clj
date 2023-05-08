69da8bacbecb331501bb59a12cd5610bca4febe1
Eitan Barylko <62489101+ebarylko@users.noreply.github.com>
2023-05-07 21:40:24 -0700

    Added introduction post (#2)
    * Created the intro post
    
    * Edited README so that it documents how to edit and maintain the blog
    
    * Edited About Me section to detail purpose of blog
    
    * Deleted example page
    
    closes #5

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cryogen "0.1.0"
-            :description "Simple static site generator"
-            :url "https://github.com/lacarmen/cryogen"
+            :description "Eitan's Blog"
+            :url "https://ebarylko.github.io"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.10.1"]
